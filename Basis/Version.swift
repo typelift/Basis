309f167ea9188a54897f07d6ba3f96dc42fe38bd
CodaFi <devteam.codafi@gmail.com>
2014-10-10 19:57:47 -0600

    Version

---
+++ Basis/Version.swift
@@ -0,0 +1,38 @@
+//
+//  Version.swift
+//  Basis-iOS
+//
+//  Created by Robert Widmann on 10/10/14.
+//  Copyright (c) 2014 Robert Widmann. All rights reserved.
+//  Released under the MIT license.
+//
+
+/// Represents the version of a piece of software.
+///
+/// Versions are equal if they have the same number, value, and ordering of branch versions and the 
+/// same tags that may not necessarily be in the same order.  
+public final class Version : K0 {
+	public let versionBranch : [Int]
+	public let versionTags : [String]
+
+	public init(_ versionBranch : [Int], _ versionTags : [String]) {
+		self.versionBranch = versionBranch
+		self.versionTags = versionTags
+	}
+}
+
+extension Version : Equatable {}
+
+public func ==(lhs : Version, rhs : Version) -> Bool {
+	return lhs.versionBranch == rhs.versionBranch && sort(lhs.versionTags) == sort(rhs.versionTags)
+}
+
+extension Version : Printable {
+	public var description : String {
+		get {
+			let versions = concat(intersperse(unpack("."))(self.versionBranch.map({ (let b : Int) in unpack(b.description) })))
+			let tags = concatMap({ (let xs : [Character]) in unpack("-") + xs })(self.versionTags.map(unpack))
+			return pack(versions + tags)
+		}
+	}
+}
---
+++ BasisTests/VersionSpec.swift
@@ -0,0 +1,22 @@
+//
+//  VersionSpec.swift
+//  Basis-iOS
+//
+//  Created by Robert Widmann on 10/10/14.
+//  Copyright (c) 2014 Robert Widmann. All rights reserved.
+//
+
+import Basis
+import XCTest
+
+
+class VersionSpec : XCTestCase {
+	func testVersion() {
+		let branches = [1,2,3]
+		let tags = ["tag1", "tag2"]
+
+		XCTAssertTrue(Version(branches, tags).description == "1.2.3-tag1-tag2", "")
+	}
+}
+
+
