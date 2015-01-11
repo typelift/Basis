3304358d88c96422c55bca07c40c2e240754f467
Robert Widmann <devteam.codafi@gmail.com>
2015-01-11 13:12:45 -0700

    Overload folds

--- Basis/Folds.swift
+++ Basis/Folds.swift
@@ -12,7 +12,7 @@
 public func foldl<A, B>(f: B -> A -> B) -> B -> [A] -> B {
 	return { z in { l in
 		switch match(l) {
-			case .Empty:
+			case .Nil:
 				return z
 			case .Cons(let x, let xs):
 				return foldl(f)(f(z)(x))(xs)
@@ -25,10 +25,36 @@ public func foldl<A, B>(f: B -> A -> B) -> B -> [A] -> B {
 public func foldl<A, B>(f: (B, A) -> B) -> B -> [A] -> B {
 	return { z in { l in
 		switch match(l) {
-			case .Empty:
+			case .Nil:
 				return z
 			case .Cons(let x, let xs):
 				return foldl(f)(f(z, x))(xs)
+		}
+	} }
+}
+
+/// Takes a binary function, a starting value, and a list of values, then folds the function over
+/// the list from left to right to yield a final value.
+public func foldl<A, B>(f: B -> A -> B) -> B -> List<A> -> B {
+	return { z in { l in
+		switch l.match() {
+		case .Nil:
+			return z
+		case .Cons(let x, let xs):
+			return foldl(f)(f(z)(x))(xs)
+		}
+	} }
+}
+
+/// Takes a binary operator, a starting value, and a list of values, then folds the function over
+/// the list from left to right to yield a final value.
+public func foldl<A, B>(f: (B, A) -> B) -> B -> List<A> -> B {
+	return { z in { l in
+		switch l.match() {
+		case .Nil:
+			return z
+		case .Cons(let x, let xs):
+			return foldl(f)(f(z, x))(xs)
 		}
 	} }
 }
@@ -45,7 +71,7 @@ public func foldl1<A>(f: A -> A -> A) -> [A] -> A {
 				return x
 			case .Cons(let x, let xs):
 				return foldl(f)(x)(xs)
-			case .Empty:
+			case .Nil:
 				assert(false, "Cannot invoke foldl1 with an empty list.")
 		}
 	}
@@ -63,8 +89,44 @@ public func foldl1<A>(f: (A, A) -> A) -> [A] -> A {
 				return x
 			case .Cons(let x, let xs):
 				return foldl(f)(x)(xs)
-			case .Empty:
+			case .Nil:
 				assert(false, "Cannot invoke foldl1 with an empty list.")
+		}
+	}
+}
+
+/// Takes a binary function and a list of values, then folds the function over the list from left
+/// to right.  It takes its initial value from the head of the list.
+///
+/// Because this function draws its initial value from the head of a list, it is non-total with
+/// respect to the empty list.
+public func foldl1<A>(f: A -> A -> A) -> List<A> -> A {
+	return { l in
+		switch l.match() {
+		case .Cons(let x, let xs) where xs.count == 0:
+			return x
+		case .Cons(let x, let xs):
+			return foldl(f)(x)(xs)
+		case .Nil:
+			assert(false, "Cannot invoke foldl1 with an empty list.")
+		}
+	}
+}
+
+/// Takes a binary operator and a list of values, then folds the function over the list from left
+/// to right.  It takes its initial value from the head of the list.
+///
+/// Because this function draws its initial value from the head of a list, it is non-total with
+/// respect to the empty list.
+public func foldl1<A>(f: (A, A) -> A) -> List<A> -> A {
+	return { l in
+		switch l.match() {
+		case .Cons(let x, let xs) where xs.count == 0:
+			return x
+		case .Cons(let x, let xs):
+			return foldl(f)(x)(xs)
+		case .Nil:
+			assert(false, "Cannot invoke foldl1 with an empty list.")
 		}
 	}
 }
@@ -74,7 +136,7 @@ public func foldl1<A>(f: (A, A) -> A) -> [A] -> A {
 public func foldr<A, B>(k: A -> B -> B) -> B -> [A] -> B {
 	return { z in { l in
 		switch match(l) {
-			case .Empty:
+			case .Nil:
 				return z
 			case .Cons(let x, let xs):
 				return k(x)(foldr(k)(z)(xs))
@@ -87,10 +149,36 @@ public func foldr<A, B>(k: A -> B -> B) -> B -> [A] -> B {
 public func foldr<A, B>(k: (A, B) -> B) -> B -> [A] -> B {
 	return { z in { l in
 		switch match(l) {
-			case .Empty:
+			case .Nil:
 				return z
 			case .Cons(let x, let xs):
 				return k(x, foldr(k)(z)(xs))
+		}
+	} }
+}
+
+/// Takes a binary function, a starting value, and a list of values, then folds the function over
+/// the list from right to left to yield a final value.
+public func foldr<A, B>(k: A -> B -> B) -> B -> List<A> -> B {
+	return { z in { l in
+		switch l.match() {
+		case .Nil:
+			return z
+		case .Cons(let x, let xs):
+			return k(x)(foldr(k)(z)(xs))
+		}
+	} }
+}
+
+/// Takes a binary operator, a starting value, and a list of values, then folds the function over
+/// the list from right to left to yield a final value.
+public func foldr<A, B>(k: (A, B) -> B) -> B -> List<A> -> B {
+	return { z in { l in
+		switch l.match() {
+		case .Nil:
+			return z
+		case .Cons(let x, let xs):
+			return k(x, foldr(k)(z)(xs))
 		}
 	} }
 }
@@ -107,7 +195,7 @@ public func foldr1<A>(f: A -> A -> A) -> [A] -> A {
 				return x
 			case .Cons(let x, let xs):
 				return f(x)(foldr1(f)(xs))
-			case .Empty:
+			case .Nil:
 				assert(false, "Cannot invoke foldr1 with an empty list.")
 		}
 	}
@@ -125,8 +213,44 @@ public func foldr1<A>(f: (A, A) -> A) -> [A] -> A {
 				return x
 			case .Cons(let x, let xs):
 				return f(x, foldr1(f)(xs))
-			case .Empty:
+			case .Nil:
 				assert(false, "Cannot invoke foldr1 with an empty list.")
+		}
+	}
+}
+
+/// Takes a binary function and a list of values, then folds the function over the list from right
+/// to left.  It takes its initial value from the head of the list.
+///
+/// Because this function draws its initial value from the head of a list, it is non-total with
+/// respect to the empty list.
+public func foldr1<A>(f: A -> A -> A) -> List<A> -> A {
+	return { l in
+		switch l.match() {
+		case .Cons(let x, let xs) where xs.count == 0:
+			return x
+		case .Cons(let x, let xs):
+			return f(x)(foldr1(f)(xs))
+		case .Nil:
+			assert(false, "Cannot invoke foldr1 with an empty list.")
+		}
+	}
+}
+
+/// Takes a binary operator and a list of values, then folds the function over the list from right
+/// to left.  It takes its initial value from the head of the list.
+///
+/// Because this function draws its initial value from the head of a list, it is non-total with
+/// respect to the empty list.
+public func foldr1<A>(f: (A, A) -> A) -> List<A> -> A {
+	return { l in
+		switch l.match() {
+		case .Cons(let x, let xs) where xs.count == 0:
+			return x
+		case .Cons(let x, let xs):
+			return f(x, foldr1(f)(xs))
+		case .Nil:
+			assert(false, "Cannot invoke foldr1 with an empty list.")
 		}
 	}
 }
@@ -147,6 +271,22 @@ public func unfoldr<A, B>(f : B -> Optional<(A, B)>) -> B -> [A] {
 	}
 }
 
+/// Takes a function and an initial seed value and constructs a list.
+///
+/// unfoldr is the dual to foldr.  Where foldr reduces a list given a function and an initial value,
+/// unfoldr uses the initial value and the function to iteratively build a list.  If list building
+/// should continue the function should return .Some(x, y), else it should return .None.
+public func unfoldr<A, B>(f : B -> Optional<(A, B)>) -> B -> List<A> {
+	return { b in
+		switch f(b) {
+		case .Some(let (a, b2)):
+			return a <| unfoldr(f)(b2)
+		case .None:
+			return List()
+		}
+	}
+}
+
 /// Returns the conjunction of a Boolean list.
 public func and(l : [Bool]) -> Bool {
 	return foldr({$0 && $1})(true)(l)
@@ -154,6 +294,16 @@ public func and(l : [Bool]) -> Bool {
 
 /// Returns the disjunction of a Boolean list.
 public func or(l : [Bool]) -> Bool {
+	return foldr({$0 || $1})(false)(l)
+}
+
+/// Returns the conjunction of a Boolean list.
+public func and(l : List<Bool>) -> Bool {
+	return foldr({$0 && $1})(true)(l)
+}
+
+/// Returns the disjunction of a Boolean list.
+public func or(l : List<Bool>) -> Bool {
 	return foldr({$0 || $1})(false)(l)
 }
 
@@ -169,6 +319,18 @@ public func all<A>(p : A -> Bool) -> [A] -> Bool {
 	return { l in and(l.map(p)) }
 }
 
+/// Maps a predicate over a list.  For the result to be true, the predicate must be satisfied at
+/// least once by an element of the list.
+public func any<A>(p : A -> Bool) -> List<A> -> Bool {
+	return { l in or(l.map(p)) }
+}
+
+/// Maps a predicate over a list.  For the result to be true, the predicate must be satisfied by
+/// all elemenets of the list.
+public func all<A>(p : A -> Bool) -> List<A> -> Bool {
+	return { l in and(l.map(p)) }
+}
+
 /// Concatenate a list of lists.
 public func concat<A>(xss : [[A]]) -> [A] {
 	return foldr({ $0 + $1 })([])(xss)
@@ -177,6 +339,16 @@ public func concat<A>(xss : [[A]]) -> [A] {
 /// Map a function over a list and concatenate the results.
 public func concatMap<A, B>(f : A -> [B]) -> [A] -> [B] {
 	return { l in foldr({ f($0) + $1 })([])(l) }
+}
+
+/// Concatenate a list of lists.
+public func concat<A>(xss : List<List<A>>) -> List<A> {
+	return foldr({ $0 + $1 })(List())(xss)
+}
+
+/// Map a function over a list and concatenate the results.
+public func concatMap<A, B>(f : A -> List<B>) -> List<A> -> List<B> {
+	return { l in foldr({ f($0) + $1 })(List())(l) }
 }
 
 /// Returns the maximum value in a list of comparable values.
@@ -193,6 +365,20 @@ public func minimum<A : Comparable>(l : [A]) -> A {
 	return foldl1(min)(l)
 }
 
+/// Returns the maximum value in a list of comparable values.
+public func maximum<A : Comparable>(l : List<A>) -> A {
+	assert(l.count != 0, "Cannot find the maximum value of an empty list.")
+
+	return foldl1(max)(l)
+}
+
+/// Returns the minimum value in a list of comparable values.
+public func minimum<A : Comparable>(l : List<A>) -> A {
+	assert(l.count != 0, "Cannot find the minimum value of an empty list.")
+
+	return foldl1(min)(l)
+}
+
 /// Returns the sum of a list of numbers.
 public func sum<N : IntegerType>(l : [N]) -> N {
 	return foldl({ $0 + $1 })(0)(l)
@@ -203,4 +389,12 @@ public func product<N : IntegerType>(l : [N]) -> N {
 	return foldl({ $0 * $1 })(1)(l)
 }
 
+/// Returns the sum of a list of numbers.
+public func sum<N : IntegerType>(l : List<N>) -> N {
+	return foldl({ $0 + $1 })(0)(l)
+}
 
+/// Returns the product of a list of numbers.
+public func product<N : IntegerType>(l : List<N>) -> N {
+	return foldl({ $0 * $1 })(1)(l)
+}
